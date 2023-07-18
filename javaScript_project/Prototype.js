e62daefbeb6f8a69bc4c2531fd4780bfba8b0f87
jahid <jahidiu@gmail.com>
2023-07-18 20:49:20 +0600

    first

--- javaScript_project/Prototype.js
+++ javaScript_project/Prototype.js
@@ -4,5 +4,14 @@ let myObj ={}
  let person1 ={
     name : 'Adam',
     age : 25,
-    
+
+ }
+
+
+ console.log(person1.hasOwnProperty('gender'));
+
+
+ function Person(_name,_age){
+        this.ane =_name
+        this.age = _age
  }
